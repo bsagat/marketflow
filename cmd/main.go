7976726df1ae024b8181a3c2c89bf9ed334738c0
bsagat <sagatbekbolat854@gmail.com>
2025-05-22 17:23:59 +0500

    feat: add API handlers, setup logic

--- cmd/main.go
+++ cmd/main.go
@@ -3,12 +3,15 @@ package main
 import (
 	"log"
 	"marketflow/internal/app"
+	"marketflow/internal/domain"
+	"net/http"
 )
 
 func main() {
-	err := app.SetConfig()
-	if err != nil {
-		log.Fatal("Config set error: ", err.Error())
-	}
+	router := app.Setup()
 
+	log.Printf("Starting server at %s... \n", *domain.Port)
+	if err := http.ListenAndServe("localhost:"+*domain.Port, router); err != nil {
+		log.Fatalf("Failed to start server: %s", err.Error())
+	}
 }
